f93bd2bb8ce0566e9dbc657f416245f0a4f2f3fd
amberlei <949144210@qq.com>
2018-04-09 17:07:54 +0800

    init project

---
+++ apsms-backend/src/main/java/com/apsms/Apsms.java
@@ -0,0 +1,12 @@
+package com.apsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apsms {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Apsms.class, args);
+    }
+}
